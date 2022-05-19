2572535efbea82fcee5a5d642a3ceaa5b5a5e56f
Jonathan Rayback <jonathan.rayback@evernym.com>
2022-05-19 16:44:59 -0600

    finish converting to functional

--- src/application/useCases/parseDid/parseDid.ts
+++ src/application/useCases/parseDid/parseDid.ts
@@ -1,10 +1,28 @@
+import * as Heket from 'heket'
 import * as Did from '../../../domain/did/did'
 
 export const ParseDid = (rawDid: string): Did.Did => {
-  Did.CheckDidSyntax(rawDid)
+  CheckDidSyntax(rawDid)
   const didParts = rawDid.split(':')
   return {
     methodName: didParts[1],
     methodSpecificId: didParts[2]
+  }
+}
+
+const CheckDidSyntax: Did.DidChecker = (rawDid: string): void => {
+  const ABNFParsingRules = `
+  did               = "did:" method-name ":" method-specific-id
+  method-name        = 1*method-char
+  method-char        = %x61-7A / DIGIT
+  method-specific-id = *( *idchar ":" ) 1*idchar
+  idchar             = ALPHA / DIGIT / "." / "-" / "_" / pct-encoded
+  pct-encoded        = "%" HEXDIG HEXDIG
+`
+  const parser = Heket.createParser(ABNFParsingRules)
+  try {
+    parser.parse(rawDid)
+  } catch (e) {
+    if (e instanceof Error) { throw new Did.MalformedDidError(e.message) }
   }
 }
