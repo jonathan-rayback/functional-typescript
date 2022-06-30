a9015dfce361822fd8d46fedde45821da41485c9
Jonathan Rayback <jonathan.rayback@evernym.com>
2022-06-30 16:27:55 -0600

    pull main execution into infrastructure layer

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,11 +1,1 @@
-import { MethodType } from '../domain/did/did'
-import { CreateDidParser } from '../application/useCases/parseDid/parseDid'
 
-const coreDidString = 'did:key:123456jysh2'
-const indyDidString = 'did:indy:sovrin:staging:5nDyJVP1NrcPAttP3xwMB9'
-
-const parseDefaultDid = CreateDidParser(MethodType.DEFAULT)
-const parseIndyDid = CreateDidParser(MethodType.INDY)
-
-console.log(parseDefaultDid(coreDidString))
-console.log(parseIndyDid(indyDidString))
